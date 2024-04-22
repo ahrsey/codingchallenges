7ef6b508c7bc6c6ed11527c5a2e6cb1a4c0c8bdc
ahrsey <richard.cargill@pm.me>
2024-04-22 01:29:03 +0100

    wc

--- wc/main.c
+++ wc/main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+void count_bytes(FILE* fp, int* b) {
+  int bytes = 0;
+  while (fgetc(fp) != EOF) ++bytes;
+  *b = bytes;
+}
+
+void count_lines(FILE* fp, int* l) {
+  int lines = 0;
+  char string[1000];
+  while (fgets(string, 1000, fp)) ++lines;
+  *l = lines;
+}
+
+void count_words(FILE* fp, int* w) {
+  int words = 0;
+  char string[1000];
+  while (fscanf(fp, "%s", string) != EOF) ++words;
+  *w = words;
+}
+
 int main(int argc, char** argv) {
   if (argc < 2) {
     printf("Requires two arguments.");
@@ -9,33 +29,28 @@ int main(int argc, char** argv) {
   char* filename = argv[2];
   FILE *fp = {0};
   fp = fopen(filename, "r");
+  int bytes = 0;
+  int lines = 0;
+  int words = 0;
 
   switch (argv[1][1]) {
     case 'c': 
       {
-        int bytes = 0;
-
-        while (fgetc(fp) != EOF) ++bytes;
-
+        count_bytes(fp, &bytes);
         printf("%d %s", bytes, filename);
+        break;
       }
     case 'l': 
       {
-        int lines = 0;
-        char string[1000];
-
-        while (fgets(string, 1000, fp)) ++lines;
-
+        count_lines(fp, &lines);
         printf("%d %s", lines, filename);
+        break;
       }
     case 'w': 
       {
-        int words = 0;
-        char string[1000];
-
-        while (fscanf(fp, "%s", string) != EOF) words++;
-
+        count_words(fp, &words);
         printf("%d %s", words, filename);
+        break;
       }
     default: break;
   }
